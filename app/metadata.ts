b62fb1da1b3d6778fde9d708e267180ed0f4fa9d
Jayesh Dhore <jayesh.dhore@icloud.com>
2025-03-23 15:35:18 +0530

    feat: update app metadata for TCET MME

--- app/metadata.ts
+++ app/metadata.ts
@@ -4,8 +4,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "TCET | Mme Attendance",
+  description: "Attendance Management System for TCET MME",
   icons: {
     icon: '/favicon.ico',
   },
